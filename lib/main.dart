a1549916ce29920942fc93b0e586bb6159d4bb0b
Túlio <francatm@outlook.com>
2019-05-21 19:15:51 -0300

    Add math lib Randomise ball number

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -28,12 +30,20 @@ class Ball extends StatefulWidget {
 }
 
 class _BallState extends State<Ball> {
+  int ballNumber = 5;
+
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Padding(
         padding: const EdgeInsets.all(32.0),
-        child: Image.asset('images/ball1.png'),
+        child: FlatButton(
+          child: Image.asset('images/ball$ballNumber.png'),
+          onPressed: () {
+            ballNumber = Random().nextInt(5);
+            print(ballNumber);
+          },
+        ),
       ),
     );
   }
