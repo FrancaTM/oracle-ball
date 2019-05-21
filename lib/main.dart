7f5eb6b06e1b827225e7f402fafbc146b5e46478
Túlio <francatm@outlook.com>
2019-05-21 18:59:43 -0300

    create stateless widget

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,21 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Scaffold(),
+      home: BallPage(),
     ),
   );
+}
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        backgroundColor: Colors.blue.shade900,
+        title: Text('Ask Me Anything'),
+      ),
+      body: Container(),
+    );
+  }
 }
